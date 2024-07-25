190df74280f2627c411a021c9dadfdc2c90df394
Hajira-hub01 <167428482+Hajira-hub01@users.noreply.github.com>
2024-07-25 10:44:35 +0000

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,9 +1,43 @@
 #include "libft.h"
+#include <stdio.h>
 
-size_t ft_strlcat (char *dst, const char *src, size_t size)
+size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t i;
+    size_t i = 0;
+    size_t j = 0;
+    size_t src_len = ft_strlen(src);
 
-    i = 0;
-    
+    while (i < size && dst[i] != '\0') 
+    {
+        i++;
+    }
+
+    if (size <= i) 
+    {
+        return size + src_len;
+    }
+
+    while (i + j < size - 1 && src[j] != '\0') 
+    {
+        dst[i + j] = src[j];
+        j++;
+    }
+
+    if (i + j < size) 
+    {
+        dst[i + j] = '\0';
+    }
+
+    return i + src_len;
+}
+
+int main() 
+{
+    char dst[4] = "Hi";
+    const char *src = "World";
+    size_t size = 4;
+
+    size_t result = ft_strlcat(dst, src, size);
+    printf("Resulting string: %s\n", dst);  
+    printf("Return value: %zu\n", result);
 }
