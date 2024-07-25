2dd9168e7e75c4922a0d1d413d7e958142805d47
Hajira-hub01 <167428482+Hajira-hub01@users.noreply.github.com>
2024-07-25 12:48:08 +0000

    libft

---
+++ ft_atoi.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	unsigned long long	nb;
+	int					sign;
+    int i;
+
+	sign = 1;
+	nb = 0;
+    i = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + str[i] - '0';
+		if (sign == -1 && nb >= LLONG_MAX)
+			return (0);
+		if (nb >= LLONG_MAX)
+			return (-1);
+		i++;
+	}
+	return (sign * nb);
+}
+
+
+int main (void)
+{
+    printf("%d\n", ft_atoi("9223372036854775807"));
+    printf("%d\n", atoi("9223372036854775807"));
+}
--- libft.h
+++ libft.h
@@ -2,16 +2,20 @@
 # define LIBFT_H
 # include <stdio.h>
 # include <unistd.h>
+# include <limits.h>
+#include <stdlib.h>
 
 int	ft_isalpha(int c);
 int ft_isalnum(int c);
 int	ft_isdigit(int c);
 int ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_atoi(const char *str)
+char *ft_strchr(const char *s, int c);
 size_t ft_strlen(const char *str);
 size_t  ft_strlcpy(char *dst, const char    *src, size_t   size);
-int	ft_toupper(int c);
-char *ft_strchr(const char *s, int c);
+size_t ft_strlcat(char *dst, const char *src, size_t size);
 
 
 # endif
