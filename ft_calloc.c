91661e335bf2e380940e3b027dca450464d586d7
Hajira Mohamed Ali <hajmoham@lab2r3s9.42abudhabi.ae>
2024-08-01 19:12:36 +0400

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,15 +2,18 @@
 
 void *ft_calloc(size_t count, size_t size)
 {
-	size_t	total_size;
+    size_t total_size;
     void *ptr;
 
+    if (size && count > (SIZE_MAX / size))
+        return (NULL);
+
+    total_size = count * size;
+
+    ptr = malloc(total_size);
     if (ptr == NULL)
-		return (NULL);
-	if (size && count > (SIZE_MAX / size))
-		return (NULL);
-	total_size = count * size;
-	ptr = malloc(total_size);
-	ft_bzero(ptr, total_size);
-	return ptr;
+        return (NULL);
+
+    ft_bzero(ptr, total_size);
+    return ptr;
 }
